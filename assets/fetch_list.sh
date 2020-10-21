39b8e5072d539bbd1f7d1cf73d1c4e4f2784e12a
Sagi Kedmi <git@sagi.io>
2020-10-21 23:22:26 +0300

    remove trailing the last trailing newline

--- assets/fetch_list.sh
+++ assets/fetch_list.sh
@@ -1,4 +1,4 @@
 curl -s https://publicsuffix.org/list/public_suffix_list.dat | \
   grep -E "^edu$|^edu\.|^ac\." | \
   sort | \
-  jq -R -s -c 'split("\n")' > educational_tlds_and_slds.json
+  jq -R -s 'split("\n") | .[0:-1]' > educational_tlds_and_slds.json
